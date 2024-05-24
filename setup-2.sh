34dabaa83586e9e254ab097b6916a5644e74f9c8
gsjurseth <gsjurseth@google.com>
2024-05-24 09:39:27 +0000

    Added tests for setup-2

--- setup-2.sh
+++ setup-2.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
-gcloud iam service-accounts create preview-gsa
-
+echo "Adding iam policy binding for workloadIdentity" >&2
 gcloud iam service-accounts add-iam-policy-binding \
 preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com \
 --role roles/iam.workloadIdentityUser \
 --member "serviceAccount:${PROJECT_ID}.svc.id.goog[apigee/preview-ksa]"
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
---member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
---role "roles/apigee.apiAdminV1"
+if [ $? -eq 0 ]
+then
+    echo "Adding policy binding for Apigee admin role" >&2
+    gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/apigee.apiAdminV1"
+else
+    echo "Failed creating policy binding for workload identity" >&2
+    exit 1
+fi
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
---member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
---role "roles/apigee.analyticsAgent"
+if [ $? -eq 0 ]
+then
+    echo "Adding policy binding for Apigee admin role" >&2
+    gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/apigee.analyticsAgent"
+else
+    echo "Failed adding policy binding for Apigee Admin Role" >&2
+    exit 1
+fi
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
---member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
---role "roles/networkservices.serviceExtensionsAdmin"
+if [ $? -eq 0 ]
+then
+    echo "Adding policy binding for service extension creation" >&2
+    gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/networkservices.serviceExtensionsAdmin"
+else
+    echo "Failed adding policy binding for Apigee Analytics agent" >&2
+    exit 1
+fi
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
---member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
---role "roles/compute.networkAdmin"
+if [ $? -eq 0 ]
+then
+    echo "Adding policy binding for network plumbing" >&2
+    gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member "serviceAccount:preview-gsa@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role "roles/compute.networkAdmin"
+else
+    echo "Failed adding policy binding for service extension creation" >&2
+    exit 1
+fi
+
+if [ $? -ne 0 ]
+then
+    echo "Failed creating policy binding for network plumbing" >&2
+fi
